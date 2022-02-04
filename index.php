88c72e183d7ccee1b4eaa4da4570d40b9377a806
ronysharif <89507734+sharifkea@users.noreply.github.com>
2022-02-04 05:16:50 +0100

    4th
    com 4

--- index.php
+++ index.php
@@ -13,8 +13,12 @@
             <h1>Welcome to SQL Project</h1>
         </header>	
     <main>
-        <div>
-            <p><a href='home.php'>>Go to Home<</a></p>	
+        <div class ="form">
+            <form action="" method="POST" name="login" >                        
+                <input id="username" placeholder="User Name" type="text" name="txtUser"  required tabindex="1"><br>
+                <input type="password" id='password' name="password" placeholder="Password" required tabindex="2"><br>
+                <input name="submit" id='submit' type="submit" value="Login" tabindex="3">
+            </form>                    
         </div>
 <?php 
         include_once('footer.php');
